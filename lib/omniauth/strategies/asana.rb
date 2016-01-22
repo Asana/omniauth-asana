9b09e4c40359d02ed96c96c50a78aaac1df13d30
polysaturate <rob.race@me.com>
2016-01-21 22:41:13 -0700

    Update asana.rb

--- lib/omniauth/strategies/asana.rb
+++ lib/omniauth/strategies/asana.rb
@@ -37,6 +37,10 @@ module OmniAuth
       def raw_info
         @raw_info ||= access_token.params['data']
       end
+      
+      def callback_url
+        options[:redirect_uri] || (full_host + script_name + callback_path)
+      end
     end
   end
 end
