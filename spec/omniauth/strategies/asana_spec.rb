a129f64bb63ef399d3a2cddf82c007f824c89760
Igor Alexandrov <igor.alexandrov@gmail.com>
2021-11-29 15:48:40 +0300

    Updated to support omniauth-2.0.

--- spec/omniauth/strategies/asana_spec.rb
+++ spec/omniauth/strategies/asana_spec.rb
@@ -1,36 +1,40 @@
 require 'spec_helper'
 
 describe OmniAuth::Strategies::Asana do
-  let(:access_token) { stub('AccessToken', :options => {}) }
-  let(:parsed_response) { stub('ParsedResponse') }
-  let(:response) { stub('Response', :parsed => parsed_response) }
+  let(:access_token) { instance_double('AccessToken', :options => {}) }
+  # let(:parsed_response) { instance_double('ParsedResponse') }
+  let(:params) { instance_double('RackParams') }
+  let(:hash) { instance_double('Hash') }
+  # let(:response) { instance_double('Response', :parsed => parsed_response) }
 
   subject do
     OmniAuth::Strategies::Asana.new({})
   end
 
   before(:each) do
-    subject.stub!(:access_token).and_return(access_token)
+    allow(subject).to receive(:access_token).and_return(access_token)
   end
 
   context "client options" do
     it 'should have correct site' do
-      subject.options.client_options.site.should eq("https://app.asana.com")
+      expect(subject.options.client_options.site).to eq("https://app.asana.com")
     end
 
     it 'should have correct authorize url' do
-      subject.options.client_options.authorize_url.should eq('https://app.asana.com/-/oauth_authorize')
+      expect(subject.options.client_options.authorize_url).to eq('https://app.asana.com/-/oauth_authorize')
     end
 
     it 'should have correct token url' do
-      subject.options.client_options.token_url.should eq('https://app.asana.com/-/oauth_token')
+      expect(subject.options.client_options.token_url).to eq('https://app.asana.com/-/oauth_token')
     end
   end
 
   context "#raw_info" do
-    it "should use relative paths" do
-      access_token.should_receive(:get).with('user').and_return(response)
-      subject.raw_info.should eq(parsed_response)
+    it "should use params" do
+      allow(access_token).to receive(:params).and_return(params)
+      allow(params).to receive(:[]).with('data').and_return(hash)
+
+      expect(subject.raw_info).to eq(hash)
     end
   end
 end
